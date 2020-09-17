42de2c264a11c2b336b983f2c5881d046f508ca3
Stephen Jensen <stephen.jensen3@gmail.com>
2020-09-17 17:48:52 -0500

    styled new feed

--- components/newFeed.tsx
+++ components/newFeed.tsx
@@ -27,27 +27,41 @@ export const NewFeed = () => {
           setFeed({ name: '', url: '', tags: [] });
         }}
       >
-        <label className="block">Name:</label>
-        <input
-          className="shadow rounded w-full py-2 px-3"
-          value={currentFeed.name}
-          onChange={e => {
-            e.persist();
-            setFeed(curr => ({ ...curr, name: e.target.value }));
-          }}
-        />
-        <label className="block">URL:</label>
-        <input
-          className="shadow rounded w-full py-2 px-3"
-          value={currentFeed.url}
-          onChange={e => {
-            e.persist();
-            setFeed(curr => ({ ...curr, url: e.target.value }));
-          }}
-        />
-        <label className="block">Tags:</label>
-        <SearchFeedTags />
-        <input className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" />
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-6 py-4">
+            <div className="py-2">
+              <label className="block py-4">Name:</label>
+              <input
+                className="shadow rounded w-full py-2 px-3"
+                value={currentFeed.name}
+                onChange={e => {
+                  e.persist();
+                  setFeed(curr => ({ ...curr, name: e.target.value }));
+                }}
+              />
+            </div>
+            <div className="py-2">
+              <label className="block py-4">URL:</label>
+              <input
+                className="shadow rounded w-full py-2 px-3"
+                value={currentFeed.url}
+                onChange={e => {
+                  e.persist();
+                  setFeed(curr => ({ ...curr, url: e.target.value }));
+                }}
+              />
+            </div>
+            <div className="py-2">
+              <input className="py-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" />
+            </div>
+          </div>
+          <div className="col-span-6 py-4">
+            <div className="py-2">
+              <label className="block py-4">Tags:</label>
+              <SearchFeedTags />
+            </div>
+          </div>
+        </div>
       </form>
     </>
   );
--- components/searchFeedTags.tsx
+++ components/searchFeedTags.tsx
@@ -17,39 +17,48 @@ export const SearchFeedTags = () => {
   const { findFeedTags } = data || {};
 
   return (
-    <>
-      <input
-        className="shadow rounded w-full py-2 px-3"
-        value={search}
-        onChange={async e => {
-          e.persist();
-          setSearch(() => e.target.value);
-          await findFeedTagsQuery({ variables: { data: { search: e.target.value } } });
-        }}
-      />
-      {loading ? (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 text-gray-500 animate-spin">
-          <path
-            fillRule="evenodd"
-            d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ) : (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 text-gray-500">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      )}
+    <div className="">
+      <div className="flex">
+        {loading ? (
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 text-gray-500 animate-spin">
+            <path
+              fillRule="evenodd"
+              d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
+              clipRule="evenodd"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="mt-3 mr-2 w-6 h-6 text-gray-500"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        )}
+        <input
+          className="shadow rounded w-full py-2 px-3"
+          value={search}
+          onChange={async e => {
+            e.persist();
+            setSearch(() => e.target.value);
+            await findFeedTagsQuery({ variables: { data: { search: e.target.value } } });
+          }}
+        />
+      </div>
+
       {findFeedTags && findFeedTags.length > 0 ? (
         findFeedTags.map(oneTag => <p key={oneTag.id}>{oneTag.name}</p>)
       ) : called ? (
         <p>No feeds found</p>
       ) : null}
-    </>
+    </div>
   );
 };
