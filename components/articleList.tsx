9847be50e5045c2aa5a689ade47735d0149aba28
Stephen Jensen <stephen.jensen3@gmail.com>
2020-12-20 11:57:49 -0600

    30. Add One Article component
    add check

--- components/articleList.tsx
+++ components/articleList.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Pagination from 'react-js-pagination';
+import { OneArticle } from './oneArticle';
 
 export const ArticleList = ({ articleList }: { articleList: any[] }) => {
   const [currentPagination, setPagination] = useState({ currentPage: 1, articlesPerPage: 8 });
@@ -8,13 +9,12 @@ export const ArticleList = ({ articleList }: { articleList: any[] }) => {
   const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
   const currentArticles = articleList.slice(indexOfFirstArticle, indexOfLastArticle);
 
-  console.log(currentArticles);
   return (
     <>
       <h3 className="py-4 font-medium text-lg ">Articles</h3>
       <div className="grid grid-cols-1 gap-4">
         {currentArticles.map(({ feed, ...oneArticle }) => (
-          <p key={oneArticle.title}>{oneArticle.title}</p>
+          <OneArticle article={oneArticle} feed={feed} key={oneArticle.title} />
         ))}
         <Pagination
           innerClass="rounded py-2 px-2 flex"
--- utils/update.ts
+++ utils/update.ts
@@ -1,4 +1,4 @@
-import { BUNDLES_QUERY, BUNDLE_QUERY, FEEDS_QUERY, FEED_QUERY } from './api/graphql/queries';
+import { BUNDLES_QUERY, BUNDLE_QUERY, FEEDS_QUERY, FEED_QUERY, SAVED_ARTICLES_QUERY, SAVED_ARTICLE_QUERY } from './api/graphql/queries';
 import * as _ from 'lodash';
 
 export const updateCache = (isFeed, action) => (store, { data }) => {
@@ -17,6 +17,25 @@ export const updateCache = (isFeed, action) => (store, { data }) => {
     store.writeQuery({
       query: isFeed ? FEEDS_QUERY : BUNDLES_QUERY,
       data: { [isFeed ? 'feeds' : 'bundles']: [...currentItems.filter(o => o.id !== item.id), item] },
+    });
+  } catch (e) {}
+};
+
+export const updateSavedArticleCache = action => (store, { data }) => {
+  const item = data[`${action}SavedArticle`];
+  try {
+    store.writeQuery({
+      query: SAVED_ARTICLE_QUERY,
+      variables: { data: { url: _.get(item, 'url') } },
+      data: { savedArticle: action == 'delete' ? null : item },
+    });
+  } catch (e) {}
+
+  try {
+    const { savedArticles } = store.readQuery({ query: SAVED_ARTICLES_QUERY });
+    store.writeQuery({
+      query: SAVED_ARTICLES_QUERY,
+      data: { savedArticles: action === 'delete' ? savedArticles.filter(o => o.id !== item.id) : [...savedArticles, item] },
     });
   } catch (e) {}
 };
