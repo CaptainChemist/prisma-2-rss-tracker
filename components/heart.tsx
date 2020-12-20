502440acc34acdb89901848a52f94149736786d7
Stephen Jensen <stephen.jensen3@gmail.com>
2020-12-20 14:18:44 -0600

    Reset unneeded files

--- components/heart.tsx
+++
@@ -1,8 +0,0 @@
-import { HeartOutline, Spin } from './svg';
-
-export const Heart = ({ size, loading, liked }: { size: number; loading: boolean; liked: boolean }) =>
-  loading ? (
-    <Spin className="h-6 w-6 text-gray-500 animate-spin inline-block align-middle" />
-  ) : (
-    <HeartOutline className={`h-${size} w-${size} ${liked ? `text-red-500` : `text-gray-500`} inline-block align-middle`} />
-  );
